3892090f76469566a78aa4c1646ca8d3dbcf00d3
erYashSurywanshi <yashhard0@gmail.com>
2025-08-29 22:53:20 +0530

    there is only a single animated model present for main section

---
+++ Myportfolio/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <main>
+      <img
+        className="absolute top-0 right-0 opacity-60 -z-10"
+        src="/gradient.png"
+        alt="Gradiant-img"
+      />
+      <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] 
+      shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
+
+      <div>
+        <Hero/>
+      </div>
+    </main>
+  );
+};
+
+export default App;
